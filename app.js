ba48d2e1cf8a4b6e94321b5cb8dff05ef87c6515
Thiago Ibiapino <thiagoibiapino21@gmail.com>
2025-01-23 16:11:28 -0300

    Alterações na função de fala que não estava funcionando

--- app.js
+++ app.js
@@ -12,10 +12,7 @@ function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
     if ('speechSynthesis' in window) {
-        let utterance = new SpeechSynthesisUtterance(texto);
-        utterance.lang = 'pt-BR'; 
-        utterance.rate = 1.2; 
-        window.speechSynthesis.speak(utterance); 
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
     } else {
         console.log("Web Speech API não suportada neste navegador.");
     }
